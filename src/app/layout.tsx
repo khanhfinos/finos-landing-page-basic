0da0c60eb0e76f569a224fe084288b4667a4f9f1
Khanh Dang <dev@earnbase.io>
2026-04-05 12:24:01 +0700

    feat: switch to Plus Jakarta Sans font for modern fintech look

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,12 @@ const spaceGrotesk = localFont({
   variable: "--font-space-grotesk",
   display: "swap",
   src: [
-    { path: "./fonts/SpaceGrotesk-Light.ttf", weight: "300", style: "normal" },
-    { path: "./fonts/SpaceGrotesk-Regular.ttf", weight: "400", style: "normal" },
-    { path: "./fonts/SpaceGrotesk-Medium.ttf", weight: "500", style: "normal" },
-    { path: "./fonts/SpaceGrotesk-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "./fonts/SpaceGrotesk-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/PlusJakartaSans-Light.ttf", weight: "300", style: "normal" },
+    { path: "./fonts/PlusJakartaSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/PlusJakartaSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/PlusJakartaSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/PlusJakartaSans-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/PlusJakartaSans-ExtraBold.ttf", weight: "800", style: "normal" },
   ],
 });
 
