90fc89a808cfc903ed4b9fa7b12cc0f685fcfcec
Khanh Dang <dev@earnbase.io>
2026-04-05 12:21:58 +0700

    fix: move fonts into src/app/fonts/ per Next.js localFont convention

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,11 @@ const spaceGrotesk = localFont({
   variable: "--font-space-grotesk",
   display: "swap",
   src: [
-    { path: "../../public/fonts/SpaceGrotesk-Light.ttf", weight: "300", style: "normal" },
-    { path: "../../public/fonts/SpaceGrotesk-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../../public/fonts/SpaceGrotesk-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../../public/fonts/SpaceGrotesk-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "../../public/fonts/SpaceGrotesk-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/SpaceGrotesk-Light.ttf", weight: "300", style: "normal" },
+    { path: "./fonts/SpaceGrotesk-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/SpaceGrotesk-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/SpaceGrotesk-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/SpaceGrotesk-Bold.ttf", weight: "700", style: "normal" },
   ],
 });
 
